cc35d4de9da2358fc76d8d652ad8fc66033969a8
GiftSibiya <giftsibiya.gb@gmail.com>
2024-03-24 21:43:50 +0200

    Search Page Text Input

--- src/screens/main/Search.tsx
+++ src/screens/main/Search.tsx
@@ -1,12 +1,29 @@
-import { View, Text } from "react-native";
+// Imports
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import React from "react";
 
 const Search = () => {
   return (
-    <View>
-      <Text>Search</Text>
+    <View style={styles.container}>
+      <TextInput style={styles.searchBar} placeholder="Search Here"></TextInput>
     </View>
   );
 };
 
 export default Search;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgb(40, 40, 40)",
+  },
+  searchBar: {
+    width: "95%",
+    height: 50,
+    backgroundColor: "white",
+    borderRadius: 10,
+    margin: 10,
+    padding: 10,
+    justifyContent: "center",
+  },
+});
